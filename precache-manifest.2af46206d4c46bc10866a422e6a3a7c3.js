671e79a2b62c2efb630103b88dc8c028db63055f
Marcin Banik <mrn.banik@gmail.com>
2019-05-29 22:49:32 +0200

    Updates

---
+++ precache-manifest.2af46206d4c46bc10866a422e6a3a7c3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "185bd0a18e5085416f65",
+    "url": "/Numbers-And-Dates-React/static/css/main.4b4948ed.chunk.css"
+  },
+  {
+    "revision": "185bd0a18e5085416f65",
+    "url": "/Numbers-And-Dates-React/static/js/main.185bd0a1.chunk.js"
+  },
+  {
+    "revision": "bc574e75b65c8acd5695",
+    "url": "/Numbers-And-Dates-React/static/js/1.bc574e75.chunk.js"
+  },
+  {
+    "revision": "3902af0ac5c99bbd46e6",
+    "url": "/Numbers-And-Dates-React/static/js/runtime~main.3902af0a.js"
+  },
+  {
+    "revision": "d8083e26979ae12cc4b7ea1d3a1ad644",
+    "url": "/Numbers-And-Dates-React/index.html"
+  }
+];
